67f3653f2744ba900279f0ae2e48d6eb5c5ba7a4
mikiangel <chinomike06@gmail.com>
2018-08-01 15:02:12 -0400

    Cambio Vista Welcome
    prueba de commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,14 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PAF
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Lista 1</a>
+                    <a href="#">Lista 2</a>
+                    <a href="#">Lista 3</a>
+                    <a href="#">Lista 4</a>
                 </div>
             </div>
         </div>
